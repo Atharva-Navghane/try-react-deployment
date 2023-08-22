1fb11ea0c86e57f932aa36fb8b302cbff4e29834
Atharva-Navghane <atharva.navghane@gmail.com>
2023-08-22 10:07:58 +0530

    Trying react deployment using github

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Atharva Navghane
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          This is just a practice run to test the deployment of a react project using Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
